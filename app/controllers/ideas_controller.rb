a9ec118546ef2a0070807a86aa67f163ff033d70
Srijal Joshi <bladedge666@gmail.com>
2016-11-23 20:18:08 +0545

    Created simple form and button styling

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,7 +3,21 @@ class IdeasController < ApplicationController
     @ideas = Idea.all
   end
 
+  def create
+    # @idea = Idea.create(description: params[:idea][:description], author: params[:idea][:description])
+    # @idea = Idea.create(params[:idea])
+    @idea = Idea.create(idea_params)
+    redirect_to root_path
+  end
 
+  def new
 
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:description, :author)
+  end
 
 end
