778e2b4cf9cd8c671f7573941ec6cf1e87fcd25e
Srijal Joshi <bladedge666@gmail.com>
2016-11-23 22:24:13 +0545

    Add the about page

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,7 @@
+class StaticController < ApplicationController
+  def about
+  end
+
+  def random
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'about' => 'static#about'
+
+  get 'random' => 'static#random'
+
   root 'ideas#index'
   resources :ideas
 end
